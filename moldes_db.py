ac4b759944249c7ac2faf95ee310fd1f05cd302e
ericcleptonsilva <ericcleptonsilva@gmail.com>
2021-05-15 13:43:39 -0300

    Finalizado tabela Inserir alunos

--- moldes_db.py
+++ moldes_db.py
@@ -1,17 +1,22 @@
 import repositorio_alunos as aluno
 import repositorio_disciplinas as disciplina
 import repositorio_notas as notas
-from consulta_tabela_disciplina import ConsultaTabelaDisciplina
+import consulta_tabela_disciplina as consult
+import random
 
 
 class Alunosdb:
     def __init__(self):
+        self.lista_d = []
         self.id = None
         self.cpf = aluno.RepoAlunos().gen_Cpf()
         self.nome = aluno.RepoAlunos().gen_nomesAlunos()
         self.email = aluno.RepoAlunos().gen_Email()
         self.matricula = aluno.RepoAlunos().gen_Matricula()
-        self.disciplina_id = ConsultaTabelaDisciplina().consultaDbDis()
+        self.disciplina_id = self.lista_d
+        for i in range(len(self.cpf)):
+            d = random.choice(consult.ConsultaTabelaDisciplina().colsutDis())
+            self.lista_d.append(d)
 
 
 class Disciplinasdb:
@@ -24,10 +29,11 @@ class Disciplinasdb:
 class Notasdb:
     def __init__(self,):
         self.aluno_id = 0
-        self.disciplina_id = 0
+        self.disciplina_id = consult.ConsultaTabelaDisciplina().colsutDis()
         self.nota_01 = notas.RepoNotas().gen_notas()
         self.nota_02 = notas.RepoNotas().gen_notas()
         self.nota_03 = notas.RepoNotas().gen_notas()
 
 
-dados = Disciplinasdb()
+dados = Alunosdb()
+print(dados.disciplina_id)
