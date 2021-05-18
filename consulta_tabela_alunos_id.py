bbcfd5257ed0ac0a0a1cc57984856bdb0ac1fccf
ericcleptonsilva <ericcleptonsilva@gmail.com>
2021-05-17 23:23:39 -0300

    Scripti  de consulta tabela alunos pronta

--- consulta_tabela_alunos_id.py
+++ consulta_tabela_alunos_id.py
@@ -1,19 +1,22 @@
 import servicesdb
+from model_consulta import *
 
 
 class ConsultaTabelaAlunoId:
-    def colsutAluId(self):
-        lista = []
+
+    def __init__(self):
+
         try:
             ''' abertura de conexao e aquisição'''
             conn = servicesdb.ConectarDB()
             '''Consulta na tabelas disciplina na coluna codigo'''
-            conn.cursordb.execute(''' SELECT id FROM alunos; ''')
+            conn.cursordb.execute(
+                ''' SELECT a.nome, a.matricula, d.nome, n.nota1, n.nota2, n.nota3 FROM alunos a
+                            JOIN notas n on n.aluno_id = a.id
+                            JOIN disciplinas d on a.disciplina_id =  d.codigo;
+                            ''')
             conn.conexaodb.commit()
-            registros = conn.cursordb.fetchall()
-            for registro in registros:
-                lista.append(registro[0])
-            return lista
+            self.registros = conn.cursordb.fetchall()
 
         except conn.conexaodb.DatabaseError as err:
 
@@ -26,30 +29,36 @@ class ConsultaTabelaAlunoId:
                 conn.cursordb.close()
         print("dados inseridos  com sucesso da tabela disciplinas!")
 
-    """Consulta os Id"""
-    def colsutDisId(self):
-        lista = []
-        try:
-            ''' abertura de conexao e aquisição'''
-            conn = servicesdb.ConectarDB()
-            '''Consulta na tabelas disciplina na coluna codigo'''
-            conn.cursordb.execute(''' SELECT disciplina_id FROM alunos; ''')
-            conn.conexaodb.commit()
-            registros = conn.cursordb.fetchall()
-            for registro in registros:
-                lista.append(registro[0])
-            return lista
+        ''' Tratamento dos dados'''
 
-        except conn.conexaodb.DatabaseError as err:
+    def ExibeDados(self):
+        print('\n Consulta ao Banco de Dados\n')
+        print('Dados da tabela - Resultato dos alunos:')
+        print('='*100)
+        print('{:<16} {:<13} {:<18} {:<9} {:<9} {:<10} {:>1} '.format(
+            'NOME ALUNO',
+            'MATRICULA',
+            'DISCIPLINAS',
+            'NOTA-01',
+            'NOTA-02',
+            'NOTA-03',
+            'SITUAÇÃO'
+        ))
+        print('-' * 100)
 
-            print("Erro de banco de dados - disciplinas", err)
+        for lista in self.registros:
+            print('{:<16} {:<10} {:<23}{:<10}{:<10}{:<10}{}'.format(
+                *lista, 'APROVADOS'))
 
-        finally:
+        print()
 
-            # fechamento de conexão
-            if conn.conexaodb:
-                conn.cursordb.close()
-        print("dados inseridos  com sucesso da tabela disciplinas!")
+        print()
+        print('='*100)
+        if (len(self.registros) == 1):
+            text = 'registro'
+        text = 'registros'
+        print('Encontrado: {} {}'.format(len(self.registros), text))
+        print('\n')
 
 
-dadosC = ConsultaTabelaAlunoId()
+dados = ConsultaTabelaAlunoId().ExibeDados()
--- repositorio_notas.py
+++ repositorio_notas.py
@@ -5,7 +5,7 @@ aluno = consultAluno.ConsultaTabelaAlunoId()
 listaId = aluno.colsutAluId()
 class RepoNotas:
     def gen_notas(self):
-        lista_n = [6.0, 7.0, 8.0, 9.0, 10.0]
+        lista_n = [4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
         lista_NN = []
         for i in range(len(listaId)):
             nota = random.choice(lista_n)
